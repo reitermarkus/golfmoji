219d1527135f1e3fe15903904e66612597f5d926
Dominik <dominik@averi.at>
2017-06-10 12:16:09 +0200

    🤔 more type struggling

--- src/golfmoji/functions.cr
+++ src/golfmoji/functions.cr
@@ -1,16 +1,26 @@
 module Golfmoji
-  FUNCTIONS = {
-    '⛳' => {
-      name: "golfmoji",
-      func: ->{ print("Hello World!") }
-    },
-    '🎲' => {
-      name: "random",
-      func: ->{ rand }
-    },
-    '⚖' => {
-      name: "compare",
-      func: ->(a, b) { a <=> b }
-    }
-  }
+    class Golfmojifunctions(A, B, R)
+        include Comparable(A)
+        include Comparable(B)
+
+        FUNCTIONS = {
+            '⛳' => {
+                name: "golfmoji",
+                func: ->{ print("Hello World!") }
+            },
+            '🎲' => {
+                name: "random",
+                func: ->{ rand }
+            },
+            '⚖' => {
+                name: "compare",
+                func: ->(a : A, b : B) { a < b if a.is_a?(Comparable) && b.is_a?(Comparable) }
+            }
+        }
+        def self.functions
+            FUNCTIONS
+        end
+    end
 end
+
+p Golfmoji::Golfmojifunctions(Int32, Int32, Int32).functions['⚖']["func"].call(5, 3)
