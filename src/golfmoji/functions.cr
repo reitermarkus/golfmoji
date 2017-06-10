d933e483f7da01d2a60f5bc44cedad465db2a78b
Markus Reiter <me@reitermark.us>
2017-06-10 16:25:48 +0200

    🔨  Use classes instead of lambdas.

--- src/golfmoji/functions.cr
+++ src/golfmoji/functions.cr
@@ -1,82 +1,65 @@
-macro num
-    Golfmoji::Num
-end
-
-macro num(val)
-    num.new({{val}})
-end
-
 module Golfmoji
-
-    class Num
-        @val : Int32 | Float64
-
-        def initialize(val)
-            @val = val
-        end
-
-        def <=>(o)
-            num(@val <=> o.val)
-        end
-
-        def val
-            @val
-        end
-
-        def inspect(io)
-            io << @val.to_s
-        end
-
-        def to_s(io)
-            io << @val.to_s
-        end
+  class Golf
+    def self.arity
+      0
     end
 
-    FUNCTIONS = {
-        '⛳' => {
-            name: "golfmoji",
-            nilad: ->{ print("Hello World!") },
-            monad: nil,
-            bylad: nil
-        },
-        '🎲' => {
-            name: "random",
-            nilad: ->{ p rand },
-            monad: nil,
-            bylad: nil
-        },
-        '⚖' => {
-            name: "compare",
-            nilad: nil,
-            monad: nil,
-            bylad: ->(a : Golfmoji::Num | Array(Golfmoji::Num), b : Golfmoji::Num | Array(Golfmoji::Num)) {
-                if a.is_a?(Array(Golfmoji::Num)) && b.is_a?(Array(Golfmoji::Num))
-                    (a.zip b).map do |e|
-                        e[0] <=> e[1]
-                    end
-                elsif a.is_a?(Array(Golfmoji::Num)) && b.is_a?(Golfmoji::Num)
-                    a.map do |e|
-                        e <=> b
-                    end
-                elsif a.is_a?(Golfmoji::Num) && b.is_a?(Array(Golfmoji::Num))
-                    b.map do |e|
-                        a <=> e
-                    end
-                elsif a.is_a?(Golfmoji::Num) && b.is_a?(Golfmoji::Num)
-                    a <=> b
-                end
-            }
-        }
-    }
-
-    def self.functions
-        FUNCTIONS
+    def self.call(*ignored)
+      "Hello World!"
     end
+
+    def self.moji
+      '⛳'
+    end
+  end
+
+  class Dice
+    def self.arity
+      0
+    end
+
+    def self.call(*ignored)
+      rand
+    end
+
+    def self.moji
+      '🎲'
+    end
+  end
+
+  class Emoji_⚖
+    def self.arity
+      2
+    end
+
+    def self.call(a : Number | String, b : Number | String, *ignored)
+      a <=> b
+    end
+
+    def self.call(a : Array(Number), b : Array(Number), *ignored)
+      (a.zip b).map { |e|
+        e[0] <=> e[1]
+      }
+    end
+
+    def self.moji
+      '⚖'
+    end
+  end
+
+  FUNCTIONS = {
+    '⛳' => Golf,
+    '🎲' => Dice,
+    '⚖' => Emoji_⚖,
+  }
+
+  def self.function(moji)
+    FUNCTIONS[moji]
+  end
 end
 
-f = Golfmoji.functions['⚖']["bylad"]
+f = Golfmoji.function('⚖')
 if f
-    p f.arity
-    p f.call(num(5), num(3))
-    p f.call([num(5), num(3), num(5)], [num(2), num(4), num(5)])
+  p f.call(5, 4)
+  p f.call([5, 3, 5], [2, 4, 5])
 end
