240d38f4e58728c4014ee887e4085d0f82760e01
Dominik <dominik@averi.at>
2017-06-14 14:45:13 +0200

    ✨ first working example

--- golfmoji.rb
+++ golfmoji.rb
@@ -1,17 +1,38 @@
 module Golfmoji
 
 	FUNCTIONS = {}
+	STACK = []
+
+	def self.put(val)
+		STACK << val
+	end
+
+	def self.peek
+		STACK.last
+	end
+
+	def self.pop
+		STACK.pop
+	end
 
 	def self.add_function(moji, func)
 		FUNCTIONS[moji] = func
 	end
 
 	def self.exec(moji)
-		FUNCTIONS[moji].call
+		FUNCTIONS[moji].call(Golfmoji)
 	end
 
-	add_function("⛳", ->{ "Hello World!" })
+	add_function("⛳", ->(s) { s.put "Hello World!" })
+	add_function("💥", ->(s) { s.put s.pop.chars })
+	add_function("©", ->(s) { s.put s.peek })
+	add_function("🎗", ->(s) { s.put s.pop.zip(s.pop) })
 
-	p exec("⛳")
+	["⛳", "💥", "📝", "🎗"].each do |f|
+		exec(f)
+	end
+
+	p "result:"
+	p peek
 
 end
