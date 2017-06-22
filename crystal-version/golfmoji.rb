ea5ef8226a6da5f0ebac1e3652a9f36fcf34e874
Markus Reiter <me@reitermark.us>
2017-06-22 13:11:40 +0200

    🚓  Run RuboCop.

--- crystal-version/golfmoji.rb
+++ crystal-version/golfmoji.rb
@@ -1,21 +1,21 @@
 FUNCTIONS = {
-    '⛳' => {
-        name: "golfmoji",
-        func: ->{ print("Hello World!\n") }
-    },
-    '🎲' => {
-        name: "random",
-        func: ->{ rand }
-    },
-    '⚖' => {
-        name: "compare",
-        func: ->(a, b) { a <=> b }
-    }
-}
+  '⛳' => {
+    name: 'golfmoji',
+    func: -> { print("Hello World!\n") }
+  },
+  '🎲' => {
+    name: 'random',
+    func: -> { rand }
+  },
+  '⚖' => {
+    name: 'compare',
+    func: ->(a, b) { a <=> b }
+  }
+}.freeze
 
-mojicode = ['⛳','🎲']
+mojicode = ['⛳', '🎲']
 
 mojicode.each do |c|
-    p FUNCTIONS[c][:func].call
-    p FUNCTIONS['⚖'][:func].call(3,2)
+  p FUNCTIONS[c][:func].call
+  p FUNCTIONS['⚖'][:func].call(3, 2)
 end
--- emolator.rb
+++ emolator.rb
@@ -1,72 +1,69 @@
 module OS
-    def self.windows?
-        (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
-    end
+  def self.windows?
+    (/cygwin|mswin|mingw|bccwin|wince|emx/ =~ RUBY_PLATFORM) != nil
+  end
 
-    def self.mac?
-        (/darwin/ =~ RUBY_PLATFORM) != nil
-    end
+  def self.mac?
+    (/darwin/ =~ RUBY_PLATFORM) != nil
+  end
 
-    def self.unix?
-        !self.windows?
-    end
+  def self.unix?
+    !windows?
+  end
 
-    def self.linux?
-        self.unix? and not self.mac?
-    end
+  def self.linux?
+    unix? && !mac?
+  end
 end
 
 src = []
 
 funcs = {
-    "0" => "⓪",
-    "1" => "①",
-    "2" => "②",
-    "3" => "③",
-    "4" => "④",
-    "5" => "⑤",
-    "6" => "⑥",
-    "7" => "⑦",
-    "8" => "⑧",
-    "9" => "⑨",
-    "+" => "➕",
-    "plus" => "➕",
-    "-" => "➖",
-    "minus" => "➖",
-    "sum" => "➕",
-    "*" => "✖",
-    "mul" => "✖",
-    "/" => "➗",
-    "div" => "➗"
+  '0' => '⓪',
+  '1' => '①',
+  '2' => '②',
+  '3' => '③',
+  '4' => '④',
+  '5' => '⑤',
+  '6' => '⑥',
+  '7' => '⑦',
+  '8' => '⑧',
+  '9' => '⑨',
+  '+' => '➕',
+  'plus' => '➕',
+  '-' => '➖',
+  'minus' => '➖',
+  'sum' => '➕',
+  '*' => '✖',
+  'mul' => '✖',
+  '/' => '➗',
+  'div' => '➗'
 }
 
-if ARGV.size > 0
+if !ARGV.empty?
+  # we have a file given. Read it or exit if the given file doesn't exist
+  unless File.exist?(ARGV.first)
+    STDERR.puts "Error: '" + ARGV.first + "' doesn't exist!"
+    exit 1
+  end
 
-    # we have a file given. Read it or exit if the given file doesn't exist
-
-    if !File::exist?(ARGV.first)
-        print("Error: '" + ARGV.first + "' doesn't exist!\n")
-        exit()
-    end
-
-    src = File::read(ARGV.first).rstrip.split("\n")
-
+  src = File.read(ARGV.first).rstrip.split("\n")
 else
+  # we don't have a file given, so we ask for input
 
-    # we don't have a file given, so we ask for input
-
-    print("Please enter the source-code you want to parse:\n(finish with an empty line)\n\n")
-    src = STDIN.gets("\n\n").rstrip.split("\n")
-
+  print("Please enter the source-code you want to parse:\n(finish with an empty line)\n\n")
+  src = STDIN.gets("\n\n").rstrip.split("\n")
 end
 
 src = src.map do |i|
-    funcs[i]
+  funcs[i]
 end
 
 if OS.mac?
-    IO.popen("pbcopy", "w") { |f| f << src.join }
-    print("Info:\nThe source-code has been copied to your clip-board! You may paste it into a new file or post it on a website.\n")
+  IO.popen('pbcopy', 'w') { |f| f << src.join }
+  puts 'Info:'
+  puts 'The source-code has been copied to your clip-board!'
+  puts 'You may paste it into a new file or post it on a website.'
 else
-    print("\n" + src.join + "\n\n")
+  print("\n" + src.join + "\n\n")
 end
